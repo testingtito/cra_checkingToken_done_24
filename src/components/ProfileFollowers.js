31c557b4019ac8f6c1016248c347bd44193b42ae
Toby Chee <wizardofqa@outlook.com>
2020-06-01 19:19:43 -0700

    Profile Followers and Following Tabs

---
+++ src/components/ProfileFollowers.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { useParams, Link } from 'react-router-dom';
+import LoadingDotsIcon from './LoadingDotsIcon';
+
+const ProfileFollowers = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const { username } = useParams();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await Axios.get(`/profile/${username}/followers`);
+        setPosts(response.data);
+        setIsLoading(false);
+      } catch (e) {
+        console.log("There was a problem.");
+      }
+    }
+    fetchPosts();
+  }, [username]);
+
+  if (isLoading) return <LoadingDotsIcon />
+  return (
+    <div className="list-group">
+      {posts.map((follower, index) => {
+        return (
+          <Link
+            key={index}
+            to={`/profile/${follower.username}`}
+            className="list-group-item list-group-item-action">
+            <img className="avatar-tiny" src={follower.avatar} /> {follower.username}
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+export default ProfileFollowers
